1fb8eb319eb4f9cf4e43f87270b3490aaaa8f526
StefanMaron <stefanmaron@outlook.de>
2023-11-04 10:16:05 +0100

    add some more buttons

--- popup.js
+++ popup.js
@@ -1,14 +1,28 @@
-function openPageInspector() {
-    window.postMessage(JSON.parse('{"type":"designer:start","data":{"designerLevel":8}}'), "https://businesscentral.dynamics.com");
-}
+document.getElementById("openPageInspection").addEventListener("click", openPageInspector);
 
-document.addEventListener('DOMContentLoaded', function () {
+function openPageInspector() {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         var activeTab = tabs[0];
         chrome.scripting.executeScript({
             target: { tabId: activeTab.id, allFrames: true },
-            func: openPageInspector,
+            func: function () {
+                window.postMessage(JSON.parse('{"type":"designer:start","data":{"designerLevel":8}}'), "https://businesscentral.dynamics.com");
+            },
         });
     });
-}, false);
+}
 
+document.getElementById("installFromAppSource").addEventListener("click", installFromAppSource);
+
+function installFromAppSource() {
+    var x;
+    var appID = prompt("Please enter the App ID you want to Install");
+    if (appID != null) {
+        window.open(`https://businesscentral.dynamics.com/?filter=%27ID%27%20IS%20%${appID}%27&page=2503`)
+    }
+}
+
+function openInVSCode() {
+
+    window.open(`vscode://ms-dynamics-smb.al/navigateTo?type=page&id=${pageID}&environmentType=Sandbox&environmentName=${sandboxName}&tenant=${tenantID}`)
+}
